8583c318021bfbb486f01d04ea6b4b39cad29159
Aba <duitbiyai2019@gmail.com>
2019-05-09 13:55:06 +0700

    Add new files

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "duitbiyai2019@gmail.com";
+$password = "Cintaku090186";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "1";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "1";
+
+
+
